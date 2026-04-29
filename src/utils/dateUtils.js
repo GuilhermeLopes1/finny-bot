0ca41ac6d1b9568080c06ee8e1ef44ae58bd0c6b
Guilherme <156544258+GuilhermeLopes1@users.noreply.github.com>
2026-04-29 09:24:50 -0300

    Add getMonthRange function to dateUtils

---
+++ src/utils/dateUtils.js
@@ -0,0 +1,13 @@
+function getMonthRange() {
+  const now = new Date();
+
+  const start = new Date(now.getFullYear(), now.getMonth(), 1);
+  const end = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
+  return {
+    start: start.toISOString(),
+    end: end.toISOString()
+  };
+}
+
+module.exports = { getMonthRange };
