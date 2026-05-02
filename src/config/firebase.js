a75c477b9be7b8516519f1d556d6cc34c3842a2c
Guilherme <156544258+GuilhermeLopes1@users.noreply.github.com>
2026-05-01 23:38:35 -0300

    Correct private key line break in firebase.js
    Fix line break in private key for Firebase service account

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,10 +2,15 @@ const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
+// 🔐 Corrige quebra de linha da chave privada
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+
+  console.log("🔥 Firebase conectado");
 }
 
 const db = admin.firestore();
