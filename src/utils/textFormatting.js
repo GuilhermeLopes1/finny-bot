38b5362ba08434b762f2fb84904a1957a20ebc4a
Guilherme <156544258+GuilhermeLopes1@users.noreply.github.com>
2026-08-04 10:38:04 -0300

    Add files via upload

--- src/utils/textFormatting.js
+++ src/utils/textFormatting.js
@@ -3,8 +3,12 @@ function cleanAllofyFormatting(value) {
     .replace(/^\s*#{1,6}\s*/gm, '')
     .replace(/\*\*([^*]+)\*\*/g, '$1')
     .replace(/__([^_]+)__/g, '$1')
+    .replace(/(^|[\s(])\*([^*\n]+)\*(?=$|[\s).,!?:;])/g, '$1$2')
+    .replace(/(^|[\s(])_([^_\n]+)_(?=$|[\s).,!?:;])/g, '$1$2')
     .replace(/^\s*[-*]\s+/gm, '• ')
     .replace(/^\s*(?:-{3,}|_{3,}|\*{3,})\s*$/gm, '')
+    .replace(/R\$\s*([\d.]+),\?/g, 'valor não confirmado ($1 reais)')
+    .replace(/[ \t]+$/gm, '')
     .replace(/\n{3,}/g, '\n\n')
     .trim();
 }
